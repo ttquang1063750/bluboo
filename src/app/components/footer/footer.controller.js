5bd6817c27d24a94c6c63bc8504cdf2134a15afb
Tang Thanh Quang <ttquang1063750@gmail.com>
2017-06-21 17:43:19 +0700

    change some style

--- src/app/components/footer/footer.controller.js
+++ src/app/components/footer/footer.controller.js
@@ -1,14 +1,5 @@
 export default class FooterController {
   constructor() {}
-
-  // on init component
-  $onInit(){
-
-  }
-
-  onSelect($event){
-    console.log($event);
-  }
 }
 
 FooterController.$inject = [];
